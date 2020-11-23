713cb907b547718848c02cd57de77510339ed7a6
TruKat <kati.k220@gmail.com>
2020-11-22 17:14:14 -0700

    UPDATE: quote can be tweeted, quote appears when pg refreshed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const spinner = document.querySelector('#js-spinner');
 const newQuoteButton = document.querySelector('#js-new-quote');
+const twitterButton = document.querySelector('#js-tweet');
 newQuoteButton.addEventListener('click', getQuote);
 
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
@@ -15,6 +16,7 @@ async function getQuote() {
     }
     const json = await response.json();
     displayQuote(json.message);
+    setTweetButton(json.message);
   } catch {
     alert('Failed to fetch new quote');
   } finally {
@@ -27,4 +29,9 @@ function displayQuote(quote) {
   const quoteText = document.querySelector('#js-quote-text');
   quoteText.textContent = quote;
 }
+
+function setTweetButton(quote) {
+    twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+  }
   
+  getQuote();
